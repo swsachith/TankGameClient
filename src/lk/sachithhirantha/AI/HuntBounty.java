02013ddfa13e7a98a5b96b270d30f3308ecf85a1
Sachith Withana <swsachith@gmail.com>
2013-07-28 03:34:05 +0530

    AI testing Started

---
+++ src/lk/sachithhirantha/AI/HuntBounty.java
@@ -0,0 +1,13 @@
+package lk.sachithhirantha.AI;
+
+import lk.sachithhirantha.controller.Engine;
+
+// this is used to hunt Bounty
+
+public class HuntBounty {
+	private Engine engine;
+	
+	public HuntBounty(){
+		engine = Engine.getInstance();
+	}
+}
---
+++ test/lk/sachithhirantha/AI/BountyHunterTester.java
@@ -0,0 +1,27 @@
+package lk.sachithhirantha.AI;
+
+import static org.junit.Assert.*;
+import lk.sachithhirantha.controller.Engine;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BountyHunterTester {
+	private Engine engine;
+	@Before
+	public void setUp() throws Exception {
+		engine = Engine.getInstance();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
